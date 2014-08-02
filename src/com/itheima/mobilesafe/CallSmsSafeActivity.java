cd2419f1e6f57e4e9f7aed022bc1ef53e4e4be47
yangqinjiang <15976543860@139.com>
2014-08-02 20:13:25 +0800

    60_listview的优化
    使用ViewHolder缓存子view的引用

--- src/com/itheima/mobilesafe/CallSmsSafeActivity.java
+++ src/com/itheima/mobilesafe/CallSmsSafeActivity.java
@@ -39,16 +39,24 @@ public class CallSmsSafeActivity extends Activity {
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
 			View view =null;
+			ViewHolder holder;
+			//1,复用历史缓存的view对象,减少view对象创建的次数
 			if(null!=convertView){
 				view = convertView;
+				holder = (ViewHolder)view.getTag();
 			}else{
+				
 				view = View.inflate(getApplicationContext(), R.layout.list_callsms_item, null);
+				//2,存储孩子view对象的引用,减少孩子查询的次数
+				holder = new ViewHolder();
+				holder.tv_mode=(TextView)view.findViewById(R.id.tv_callsms_mode);
+				holder.tv_phone = (TextView)view.findViewById(R.id.tv_callsms_phone);
+				//放进父view的包里
+				view.setTag(holder);
 			}
 			
-			TextView tv_phone = (TextView)view.findViewById(R.id.tv_callsms_phone);
-			TextView tv_mode = (TextView)view.findViewById(R.id.tv_callsms_mode);
 			BlackNumberInfo blackNumberInfo = blacknumberinfos.get(position);
-			tv_phone.setText(blackNumberInfo.getPhone());
+			holder.tv_phone.setText(blackNumberInfo.getPhone());
 			String mode = blackNumberInfo.getMode();
 			
 			if("1".equals(mode)){
@@ -58,7 +66,7 @@ public class CallSmsSafeActivity extends Activity {
 			}else{
 				mode="全部拦截";
 			}
-			tv_mode.setText(mode);
+			holder.tv_mode.setText(mode);
 			return view;
 		}
 
@@ -73,5 +81,13 @@ public class CallSmsSafeActivity extends Activity {
 		}
 
 		
+	}
+	/**
+	 * 定义一个记事本,存放孩子的引用
+	 *
+	 */
+	static class ViewHolder{//静态类,
+		TextView tv_mode;
+		TextView tv_phone;
 	}
 }

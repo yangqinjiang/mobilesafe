15f01c5f417bc9db3166082dd2405d114170b21c
yangqinjiang <15976543860@139.com>
2014-08-03 21:39:13 +0800

    66_黑名单电话拦截的原理

--- src/com/itheima/mobilesafe/service/CallSmsSafeService.java
+++ src/com/itheima/mobilesafe/service/CallSmsSafeService.java
@@ -8,13 +8,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
+import android.telephony.PhoneStateListener;
 import android.telephony.SmsMessage;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 /**
  * 短信的黑名单拦截
  *
  */
 public class CallSmsSafeService extends Service {
+	private TelephonyManager tm;
 private class InnerSmsReceiver extends BroadcastReceiver{
 
 	private static final String TAG = "InnerSmsReceiver";
@@ -33,6 +36,11 @@ private class InnerSmsReceiver extends BroadcastReceiver{
 				Log.i(TAG, "发现黑名单短信,拦截");
 				abortBroadcast();//确保权限值最高,第一个接收到短信
 			}
+			String body = smsMessage.getMessageBody();
+			if(body.contains("fapiao")){
+				Log.i(TAG, "发现垃圾短信,拦截");
+				abortBroadcast();//确保权限值最高,第一个接收到短信
+			}
 		}
 	}
 	
@@ -43,8 +51,13 @@ private class InnerSmsReceiver extends BroadcastReceiver{
 	}
 	private InnerSmsReceiver receiver;
 	private BlackNumberDao dao;
+	private MyPhoneListener listener;
 	@Override
 	public void onCreate() {
+		tm =(TelephonyManager)getSystemService(TELEPHONY_SERVICE);
+		listener = new MyPhoneListener();
+		//监听电话呼叫的状态
+		tm.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
 		dao = new BlackNumberDao(this);
 		receiver = new InnerSmsReceiver();
 		IntentFilter filter = new IntentFilter();
@@ -58,6 +71,34 @@ private class InnerSmsReceiver extends BroadcastReceiver{
 		public void onDestroy() {
 		unregisterReceiver(receiver);
 		receiver=null;
+		//
+		tm.listen(listener, PhoneStateListener.LISTEN_NONE);
+		listener=null;
+	}
+	class MyPhoneListener extends PhoneStateListener{
+		private static final String TAG = "MyPhoneListener";
+
+		@Override
+		public void onCallStateChanged(int state, String incomingNumber) {
+			switch (state) {
+			case TelephonyManager.CALL_STATE_IDLE:
+				
+				break;
+			case TelephonyManager.CALL_STATE_RINGING:
+				//检查号码是否有黑名单号码
+				String mode = dao.findMode(incomingNumber);//1:电话拦截,2,短信拦截,3:全部拦截
+				if("1".equals(mode)|| "3".equals(mode)){
+					Log.i(TAG, "发现黑名单电话,拦截");
+					//在这里挂断电话
+				}
+				break;
+			case TelephonyManager.CALL_STATE_OFFHOOK:
+				
+				break;
+			default:
+				break;
+			}
+		}
 	}
 
 }
